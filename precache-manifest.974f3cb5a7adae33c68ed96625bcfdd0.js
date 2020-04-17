072c5b523644b3fa575fa70e8ba5e31e2e7cddcd
aswaff <44035215+aswaff@users.noreply.github.com>
2020-04-17 13:26:15 -0400

    Updates

---
+++ precache-manifest.974f3cb5a7adae33c68ed96625bcfdd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded7aa22995c46fdc660ce7234b8e8b",
+    "url": "/URL-Shortener/index.html"
+  },
+  {
+    "revision": "71e1441cb2dd67c41c51",
+    "url": "/URL-Shortener/static/css/main.a2ca4051.chunk.css"
+  },
+  {
+    "revision": "6984c111d6b060530ba2",
+    "url": "/URL-Shortener/static/js/2.65747866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/URL-Shortener/static/js/2.65747866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e1441cb2dd67c41c51",
+    "url": "/URL-Shortener/static/js/main.2287edec.chunk.js"
+  },
+  {
+    "revision": "99eb67cc26a80550f06d",
+    "url": "/URL-Shortener/static/js/runtime-main.3b77256a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/URL-Shortener/static/media/logo.5d5d9eef.svg"
+  }
+]);
